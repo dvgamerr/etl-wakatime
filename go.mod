5c38e97ddc24f643e9930fe452a976f647d2cf13
Kananek Thongkam <info.dvgamer@gmail.com>
2023-11-01 23:12:42 +0700

    refactor: cmd func

--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.21.3
 
 require (
 	github.com/alexflint/go-arg v1.4.3
+	github.com/hairyhenderson/go-which v0.2.0
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/zap v1.26.0
 )
 
 require (
 	github.com/alexflint/go-scalar v1.2.0 // indirect
+	github.com/spf13/afero v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"bytes"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 
 	"github.com/alexflint/go-arg"
+	"github.com/hairyhenderson/go-which"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
 
-var logger *zap.Logger
+var (
+	logger *zap.Logger
+	sugar  *zap.SugaredLogger
+)
+
+func win64Goos(path string) string {
+	if runtime.GOOS == "windows" && filepath.Ext(path) == "" {
+		return path + ".exe"
+	}
+	return path
+}
 
 func init() {
-	logger, _ = zap.NewDevelopment()
-	if _, err := os.Stat(".env"); err != nil {
-		return
+	if _, err := os.Stat(".env"); err == nil {
+		logger, _ = zap.NewDevelopment()
+		err := godotenv.Load()
+		if err != nil {
+			logger.Fatal(err.Error())
+		}
+	} else {
+		logger, _ = zap.NewProduction()
 	}
-	err := godotenv.Load()
-	if err != nil {
-		logger.Fatal(err.Error())
+	sugar = logger.Sugar()
+
+	if !which.Found(win64Goos("duckdb")) {
+		logger.Fatal("Required duckdb command 'https://duckdb.org/#quickinstall' ")
+	}
+	if !which.Found(win64Goos("psql")) {
+		logger.Fatal("Required psql command 'https://www.postgresql.org/download/' ")
 	}
 }
 
@@ -30,7 +54,6 @@ type WakaETLConfig struct {
 
 func main() {
 	defer logger.Sync() // flushes buffer, if any
-	sugar := logger.Sugar()
 
 	var etl WakaETLConfig
 	arg.MustParse(&etl)
@@ -44,4 +67,18 @@ func main() {
 	} else {
 		sugar.Fatalw("output not supported.")
 	}
+}
+
+func cmd(name string, arg ...string) error {
+	var stdout bytes.Buffer
+	cmd := exec.Command(name, arg...)
+	// cmd.Stdin = strings.NewReader("and old falcon")
+
+	cmd.Stdout = &stdout
+	if err := cmd.Run(); err != nil {
+		sugar.Debugf("%q\n", stdout.String())
+		return err
+	}
+
+	return nil
 }
